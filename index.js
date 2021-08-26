909f8614f041cf40d26704b9c19714377c6ac7e3
Bartosz Galek <bartosz.galek@allegro.pl>
2021-08-26 10:51:48 +0200

    initial version

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const core = require('@actions/core');
-const action = require('./action');
+const fetch = require('node-fetch');
 
-const result = action();
+const host = core.getInput('host');
+const identity = core.getInput('identity');
+const name = core.getInput('name');
+const group = core.getInput('group');
+const version = core.getInput('version');
+const username = core.getInput('username');
+const password = core.getInput('password');
+const user = process.env['GITHUB_ACTOR'];
 
-core.setOutput('result', result);
+const headers = { headers: { Authorization: `Basic ${Buffer.from(`${username}:${password}`, 'binary').toString('base64')}` } };
+
+fetch(`http://${host}/access-token`, headers)
+  .then(response => response.json())
+  .then(json => json.token)
+  .then(token => fetch(`http://${host}/identities/${identity}/services`, {
+    body: JSON.stringify({
+      changes: [
+        {
+          operation: 'upsert',
+          artifact: {
+            groupId: group,
+            artifactId: name,
+            version: version
+          }
+        }
+      ]
+    }),
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      'user': user
+    },
+    method: 'PATCH'
+  }))
+  .then(response => response.json())
+  .then(json => core.info(json));
